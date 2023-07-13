58584df6b51f3c6586b5ed2a41726cf9ba0a5942
MikeBilbee <cmbilbee@gmail.com>
2023-07-13 12:38:31 -0700

    4.5 please

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -27,9 +27,9 @@ def python(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    return "{:d} is a number".format(n)
+    return ("{} is a number".format(n))
 
 
 if __name__ == '__main__':
