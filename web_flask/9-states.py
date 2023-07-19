0a639b81e4a69716c4555043e004204a41f87cd2
MikeBilbee <cmbilbee@gmail.com>
2023-07-19 08:03:20 -0700

    10.0

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that starts a Flask web application 
-to load all cities of a state 
+Write a script that starts a Flask web application
+to load all cities of a state
 """
 from flask import Flask, render_template
 from models import storage, State
@@ -15,11 +15,11 @@ def teardown_appcontext(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states', strict_slashes=False)
 def states_list():
     """displays list of states"""
     states = storage.all("State")
-    return render_template('7-states_list.html', states=states.values())
+    return render_template('9-states.html', not_found=True)
 
 
 @app.route('/states/<id>', strict_slashes=False)
@@ -29,8 +29,6 @@ def states_id():
     if state:
         cities = sorted(state.cities, key=lambda city: city.name)
         return render_template('9-states.html', state=state, cities=cities)
-    else:
-        return render_template('9-states.html', not_found=True)
 
 
 if __name__ == "__main__":
